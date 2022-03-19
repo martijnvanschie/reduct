0955b3d1bd238a04aa5013ab39902a4a79b7318f
Martijn van Schie <martijnvanschie@gmail.com>
2022-03-19 17:28:45 +0100

    Moved Reduct.Azure.EventGrid to framework

---
+++ Reduct.Azure.EventGrid/EventPublisher.cs
@@ -0,0 +1,35 @@
+﻿using Azure;
+using Azure.Messaging.EventGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reduct.Azure.EventGrid
+{
+    public class EventPublisher
+    {
+        EventGridPublisherClient client;
+
+        public EventPublisher(string topicName, string region, string accessKey)
+        {
+            client = new EventGridPublisherClient(
+                new Uri($"https://{topicName}.{region}.eventgrid.azure.net/api/events"),
+                new AzureKeyCredential(accessKey)
+                );
+        }
+
+        public async Task PublishBinaryDataAsync(BinaryData eventBinary, string? eventId = null)
+        {
+            var eventData = EventGridEvent.Parse(eventBinary);
+
+            if (eventId != null)
+            {
+                eventData.Id = eventId;
+            }
+
+            await client.SendEventAsync(eventData);
+        }
+    }
+}
